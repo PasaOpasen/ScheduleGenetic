8a628f9c2bb79367c4210b13bfd6ab7f74b89540
ДМ. Па <qtckpuhdsa@gmail.com>
2021-05-29 22:02:42 +0300

    +

---
+++ algo/read_data.py
@@ -0,0 +1,158 @@
+
+
+import numpy as np
+import pandas as pd
+
+
+from groups import Group
+
+#import sys, os
+#sys.path.append(os.path.dirname(file))
+
+def df_exceptions_dict(df):
+    
+    answer = {}
+    indexes = np.arange(1, df.shape[1])
+
+    for i in range(df.shape[0]):
+        answer[df.iloc[i,0]] = indexes[df.values[i,1:] == 0]
+    
+    return {key: list(val) for key, val in answer.items()}
+
+
+
+def read_teachers_schedule(file_name = '../data/Diss.xlsx'):
+
+    df = pd.read_excel(file_name, sheet_name = 'Schedule teachers').iloc[3:,1:].fillna(-1)
+    
+    df.columns = [df.iloc[0,0]] + [str(int(v)) for v in df.iloc[0,1:]]
+    
+    df = df.iloc[1:,:]
+    
+    return df
+
+
+def read_places_schedule(file_name = '../data/Diss.xlsx'):
+
+    df = pd.read_excel(file_name, sheet_name = 'Schedule places').iloc[2:,1:].fillna(-1)
+        
+    df.columns = [df.iloc[0,0]] + [str(int(v)) for v in df.iloc[0,1:]]
+        
+    df = df.iloc[1:,:]
+    
+    return df
+
+
+def read_groups_schedule(file_name = '../data/Diss.xlsx'):
+    
+    df = pd.read_excel(file_name, sheet_name = 'Schedule groups').iloc[3:,1:].fillna(-1)
+        
+    df.columns = [df.iloc[0,0]] + [str(int(v)) for v in df.iloc[0,1:]]
+        
+    df = df.iloc[1:,:]
+    
+    return df
+
+
+
+
+
+def read_teachers(file_name = '../data/Diss.xlsx'):
+    
+    df = pd.read_excel(file_name, sheet_name = 'Teachers')
+    
+    data = {key: [val.strip() for val in val.split(';') if val] for key, val in zip(df.iloc[:,0], df.iloc[:,1])}
+    
+    return data
+
+
+def read_places(file_name = '../data/Diss.xlsx'):
+    
+    df = pd.read_excel(file_name, sheet_name = 'Places')
+    
+    places = df.iloc[:,0]
+    capacitys = df.iloc[:,1]
+    contains = [set([]) if v is np.nan else set([vl.strip() for vl in v.split(',') if vl]) for v in df.iloc[:,2]] 
+
+    
+    return dict(zip(places, capacitys)), dict(zip(places, contains))
+
+
+def read_subjects(file_name = '../data/Diss.xlsx'):
+    
+    df = pd.read_excel(file_name, sheet_name = 'Subjects')
+    
+    subjects = df.iloc[:,0]
+    reqs = [set([]) if v is np.nan else set([vl.strip() for vl in v.split(',') if vl]) for v in df.iloc[:,2]]
+    difc =  df.iloc[:,3]  
+    hours = df.iloc[:,4]
+
+    
+    return dict(zip(subjects, reqs)), dict(zip(subjects, difc)), dict(zip(subjects, hours))
+
+
+def read_groups(file_name = '../data/Diss.xlsx'):
+    
+    df = pd.read_excel(file_name, sheet_name = 'Groups')
+    
+    code = df.iloc[:,0]
+    names = df.iloc[:,1]
+    childs = [None if v is np.nan else [int(vl.strip()) for vl in v.split() if vl] for v in df.iloc[:,2]] 
+    capacitys =  df.iloc[:,3]  
+
+
+    return dict(zip(code, names)), dict(zip(code, capacitys)), [Group(id_, childs_) for id_, childs_ in zip(code, childs)]
+
+
+
+
+def read_all_data(file_name = '../data/Diss.xlsx'):
+
+    answer = {}
+
+    answer['teacher_schedule'] = read_teachers_schedule(file_name)
+    answer['teacher_exceptions'] = df_exceptions_dict(answer['teacher_schedule'])
+
+    answer['places_schedule'] = read_places_schedule(file_name)
+    answer['places_exceptions'] = df_exceptions_dict(answer['places_schedule'])
+
+    answer['groups_schedule'] = read_groups_schedule(file_name)
+    answer['groups_exceptions'] = df_exceptions_dict(answer['groups_schedule'])
+
+    answer['teachers'] = read_teachers(file_name)
+
+    answer['places2counts'], answer['places2contain'] = read_places(file_name)
+
+    answer['subjects2reqs'], answer['subjects2diffc'], answer['subjects2hours'] = read_subjects(file_name)
+
+    answer['groups2names'], answer['groups2counts'], answer['all_groups'] = read_groups(file_name)
+
+    return answer
+
+
+
+
+
+if __name__ == '__main__':
+
+    result = read_all_data()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ algo/valid_data.py
@@ -0,0 +1,75 @@
+
+import itertools
+
+import numpy as np
+import pandas as pd
+
+
+
+def get_all_combinations_df(result):
+    """
+    выдает все возможные допустимые комбинации препод-предмет-место-группа
+
+    """
+
+    # дальше я всё делаю не прям в один шаг, чтоб не расходовать дико много памяти
+
+    # все комбинации учителей и их предметов
+    teachers_subjects = sum([list(itertools.product([key], val)) for key, val in result['teachers'].items()], [])
+
+    # все комбинации групп и аудиторий, но чтобы аудитория вмещала группу
+    places_groups = sum([ [(place, group) for group, count2 in result['groups2counts'].items() if count1 >= count2] for place, count1 in result['places2counts'].items()], [])
+
+
+    teachers_subjects_places_groups = list(itertools.product(teachers_subjects, places_groups))
+
+    def connect(place, subject):
+        pl = result['places2contain'][place]
+        su = result['subjects2reqs'][subject]
+
+        return len(pl.intersection(su)) == max((len(pl), len(su)))
+
+
+    teachers_subjects_places_groups = [(teacher, subject, place, group) for (teacher, subject), (place, group) in teachers_subjects_places_groups if connect(place, subject)]
+
+    return pd.DataFrame(teachers_subjects_places_groups, columns = ['teacher', 'subject', 'place', 'group'])
+
+
+def get_ways_for_timeslots(result):
+    """
+    возвращает все допустимые комбинации вообще и номера разрешенных комбинаций под каждый слот
+    """
+
+    all_combs = get_all_combinations_df(result)
+
+    slots = dict.fromkeys(range(1, result['teacher_schedule'].shape[1]))
+
+    all_rows = np.arange(all_combs.shape[0])
+
+    
+    def get_keys_with_elem(dct, elem):
+        return [key for key, val in dct.items() if elem in val]
+
+
+    for slot in list(slots.keys()):
+        
+        group_ex = get_keys_with_elem(result['groups_exceptions'], slot)
+        teacher_ex = get_keys_with_elem(result['teacher_exceptions'], slot)
+        place_ex = get_keys_with_elem(result['places_exceptions'], slot)
+
+        slots[slot] = all_rows[~(all_combs['teacher'].isin(teacher_ex) | all_combs['place'].isin(place_ex) | all_combs['group'].isin(group_ex))]
+    
+    return all_combs, slots
+
+
+
+
+
+
+
+
+
+
+
+
+
